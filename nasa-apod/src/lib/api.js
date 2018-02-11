87c1edcbb0139eb84623adc0cb3d3d399bcfd8a8
dongheecha <ddonghee93@naver,com>
2018-02-11 18:55:07 +0900

    git init

--- nasa-apod/src/lib/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export function getAPOD(date = '') {
-  return axios.get(`https://api.nasa.gov/planetary/apod?api_key=cx443cn2l6IE7UmdyFlG534oSifee8jVQs0OLMz3&date=${date}`);
-}
